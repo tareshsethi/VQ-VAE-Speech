328e6e8e9f8ba4255e6091400593f6e09b6ff557
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-12-12 06:13:17 +0000

    visualization

--- src/export.py
+++ src/export.py
@@ -6,11 +6,8 @@ with open('train.pickle', 'rb') as input_file:
 
 wav_names = object_dict.keys()
 X = np.zeros((2800, 1536))
-# Y = np.empty([2800, 1], dtype=object)
 Y = np.zeros((2800, 1))
 names = {'happy.wav':0, 'sad.wav':1, 'angry.wav':2, 'disgust.wav':3, 'ps.wav':4, 'fear.wav':5, 'neutral.wav':6}
-
-# names = ['happy.wav', 'sad.wav', 'angry.wav', 'disgust.wav', 'ps.wav', 'fear.wav', 'neutral.wav']
 
 for i, wav in enumerate(wav_names):
     # print(object_dict[wav]['quantized'].squeeze().reshape(1, -1).shape)
@@ -21,15 +18,9 @@ for i, wav in enumerate(wav_names):
             X[i] = object_dict[wav]['quantized'].squeeze().flatten().cpu().detach().numpy()
             break
 
-print (type(X))
-print (X)
-# X = np.concatenate(X, axis=1)
-# Y = np.concatenate(Y, axis=0)
-
 print (X.shape)
 Y = np.asarray(Y)
-print(Y)
-# print(np.asarray(X).shape)
+print(Y.shape)
 
 with open('X.pickle', 'wb') as output:
     pickle.dump(X, output)
--- src/plot.py
+++ src/plot.py
@@ -6,7 +6,6 @@ import seaborn as sns
 import matplotlib.cm as cm
 
 sns.set(style='white', context='notebook', rc={'figure.figsize':(14,10)})
-colors = {0:'happy.wav', 1:'sad.wav', 2:'angry.wav', 3:'disgust.wav', 4:'ps.wav', 5:'fear.wav', 6:'neutral.wav'}
 color_labels = ['happy', 'sad', 'angry', 'disgust', 'pleasant surprise', 'fear', 'neutral']
 
 with open('X.pickle', 'rb') as x:
@@ -22,7 +21,6 @@ reducer = umap.UMAP()
 embedding = reducer.fit_transform(X)
 
 print(embedding.shape)
-# print([sns.color_palette()[int(x)] for x in Y.squeeze()])
 
 fig, ax = plt.subplots()
 pts = ax.scatter(embedding[:, 0], embedding[:, 1], c=[int(x) for x in Y.squeeze()], cmap='Spectral')
